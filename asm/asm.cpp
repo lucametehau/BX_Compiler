c099b0e92639d905813c64b42a21ab526a6651f3
lucametehau <31002793+lucametehau@users.noreply.github.com>
2025-09-29 00:16:18 +0200

    Should be working, I forgot to merge

--- asm/asm.cpp
+++ asm/asm.cpp
@@ -5,16 +5,18 @@ namespace ASM {
 Assembler::Assembler(std::vector<TAC>& _instr) : instr(std::move(_instr)) {
     stack_size = 0;
     for (auto &t : instr) {
-        if (t["opcode"][0] != "label") {
-            for (auto &temp : t["args"]) {
-                if (temp[0] == '%')
-                    stack_size = std::max(stack_size, static_cast<std::size_t>(std::stoi(temp.substr(1))));
-            }
+        auto args = t["args"];
+        auto result = t["result"];
+        
+        for (auto &temp : args) {
+            if (temp[0] == '%' && temp[1] != '.')
+                stack_size = std::max(stack_size, static_cast<std::size_t>(std::stoi(temp.substr(1))));
+        }
 
-            if (!t["result"].empty()) {
-                if (t["result"][0][0] == '%')
-                    stack_size = std::max(stack_size, static_cast<std::size_t>(std::stoi(t["result"][0].substr(1))));
-            }
+        if (!result.empty()) {
+            std::cout << result[0] << "\n";
+            if (result[0][0] == '%' && result[0][1] != '.')
+                stack_size = std::max(stack_size, static_cast<std::size_t>(std::stoi(t["result"][0].substr(1))));
         }
     }
 }
@@ -57,13 +59,42 @@ void Assembler::assemble_instr(std::ofstream& os, TAC& tac) {
     }
     else if (op == "print") {
         assert(args.size() == 1);
+#ifdef _WIN32
+        os << "\tmovq " << stack_register(args[0]) << ", %rcx\n";
+        os << "\tcallq bx_print_int\n";
+#else
         os << "\tmovq " << stack_register(args[0]) << ", %rdi\n";
         os << "\tcallq bx_print_int\n";
+#endif
     }
     else if (op == "jmp") {
         assert(args.size() == 1 || (args.empty() && result.size() == 1));
         auto label = args.empty() ? result[0] : args[0];
         os << "\tjmp " << label.substr(1) << "\n";
+    }
+    else if (jumps.count(op)) {
+        assert(args.size() == 1 && result.size() == 1);
+        os << "\tcmpq $0, " << stack_register(args[0]) << "\n";
+        os << "\t" << op << " " << result[0].substr(1) << "\n";
+    }
+    else if (auto it = uniops.find(op); it != uniops.end()) {
+        assert(args.size() == 1 && result.size() == 1);
+        os << "\tmovq " << stack_register(args[0]) << ", %r8\n";
+        os << "\t" << it->second << " %r8\n";
+        os << "\tmovq %r8, " << stack_register(result[0]) << "\n";
+    }
+    else if (auto it = normal_binops.find(op); it != normal_binops.end()) {
+        assert(args.size() == 2 && result.size() == 1);
+        os << "\tmovq " << stack_register(args[0]) << ", %r8\n";
+        os << "\t" << it->second << " " << stack_register(args[1]) << ", %r8\n";
+        os << "\tmovq %r8, " << stack_register(result[0]) << "\n";
+    }
+    else if (auto it = special_binops.find(op); it != special_binops.end()) {
+        assert(args.size() == 2 && result.size() == 1);
+        it->second(args[0], args[1], result[0], os);
+    }
+    else {
+        throw std::runtime_error("Unrecognized operator " + op);
     }
 }
 
--- asm/asm.h
+++ asm/asm.h
@@ -1,6 +1,9 @@
 #pragma once
-#include "tac.h"
+#include "../mm/tac.h"
 #include <fstream>
+#include <set>
+#include <map>
+#include <functional>
 
 namespace ASM {
 
@@ -17,10 +20,55 @@ public:
 private:
     std::string stack_register(std::string& temp) {
         assert(temp[0] == '%');
-        return "-" + std::stoi(temp.substr(1)) + "(%rbp)";
+        std::cout << temp.substr(1) << "\n";
+        return "-" + std::to_string(8 * std::stoi(temp.substr(1))) + "(%rbp)";
     }
-    
+
     void assemble_instr(std::ofstream& os, TAC& tac);
+};
+
+static const std::set<std::string> jumps = {
+    "jz", "jnz", "jl", "jle", "jg", "jge"
+};
+
+static const std::map<std::string, std::string> uniops = {
+    {"neg", "negq"}, {"not", "notq"}
+};
+
+static const std::map<std::string, std::string> normal_binops = {
+    {"add", "addq"}, {"sub", "subq"}, {"and", "andq"}, {"or", "orq"}, {"xor", "xorq"}
+};
+
+static const std::map<std::string, std::function<void(std::string, std::string, std::string, std::ofstream&)>> special_binops = {
+    {"mul", [](std::string a, std::string b, std::string res, std::ofstream& os) {
+        os << "\tmovq " << a << ", %rax\n";
+        os << "\timulq " << b << "\n";
+        os << "\tmovq %rax, " << res << "\n";
+    }},
+    {"div", [](std::string a, std::string b, std::string res, std::ofstream& os) {
+        os << "\tmovq " << a << ", %rax\n";
+        os << "\tcqto\n";
+        os << "\tidivq " << b << "\n";
+        os << "\tmovq %rax, " << res << "\n";
+    }},
+    {"mod", [](std::string a, std::string b, std::string res, std::ofstream& os) {
+        os << "\tmovq " << a << ", %rax\n";
+        os << "\tcqto\n";
+        os << "\tidivq " << b << "\n";
+        os << "\tmovq %rdx, " << res << "\n";
+    }},
+    {"shl", [](std::string a, std::string b, std::string res, std::ofstream& os) {
+        os << "\tmovq " << a << ", %r8\n";
+        os << "\tmovq " << b << ", %rcx\n";
+        os << "\tsalq %cl, %r8\n";
+        os << "\tmovq %r8, " << res << "\n";
+    }},
+    {"shr", [](std::string a, std::string b, std::string res, std::ofstream& os) {
+        os << "\tmovq " << a << ", %r8\n";
+        os << "\tmovq " << b << ", %rcx\n";
+        os << "\tsarq %cl, %r8\n";
+        os << "\tmovq %r8, " << res << "\n";
+    }}
 };
 
 };
